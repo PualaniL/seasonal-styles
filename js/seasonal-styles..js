0c54fa11d79cf6fa6604ad8b1124623aa93e0b6b
PualaniL <130610369+PualaniL@users.noreply.github.com>
2023-08-01 23:23:22 -0700

    final updates

--- js/seasonal-styles..js
+++ js/seasonal-styles..js
@@ -1,43 +1,46 @@
-$(document).ready(function(){
-    var date = new Date();
-    var month = date.getMonth();
-    
-    var season;
-    if (month >= 2 && month <= 4){
-        season = 'Spring';
-    } else if (month >= 5 && month <= 7){
-        season = 'Summer';
-    } else if (month >= 8 && month <= 10){
-        season = 'Autumn';
-    } else {
-        season = 'Winter';
-    }
-    
-    switch(season){
-        case "spring":
-            $("html").css("background-color", "#2B7129");
-            $("#slogan").text("Spring into our Spring wear!");
-            $("#wear").attr("src", "images/spring-wear.jpg");
-            $("#logo").attr("src", "images/spring.gif");
+$("document").ready(function(){
+    $('.seasons a').click(function(e){//find all a tags inside class of seasons
+        e.preventDefault();//stop default submission
+        let season = $(this).attr("href");//contents of href attribute of this element
+        //alert(season);
+
+        season = season.toLowerCase();
+
+        switch(season){
+            case "spring":
+                $("html").css("background-color","#2B7129");
+                $("#slogan").text("Bloom into Spring Fashion!")
+                $("#wear").attr("src","images/spring-wear.jpg");
+                $("#logo").attr("src","images/spring.gif");
         break;
+
         case "summer":
             $("html").css("background-color", "#EBA52B");
-            $("#slogan").text("Feel cool in our Summer wear!");
+            $("#slogan").text("Stay Cool, Look Hot.");
             $("#wear").attr("src", "images/summer-wear.jpg");
             $("#logo").attr("src", "images/summer.gif");
         break;
-        case "fall":
-            $("html").css("background-color", "#A81124");
-            $("#slogan").text("Fall into our Fall wear!");
-            $("#wear").attr("src", "images/fall-wear.jpg");
-            $("#logo").attr("src", "images/fall.gif");
-        break;
+
         case "winter":
             $("html").css("background-color", "#005393");
-            $("#slogan").text("Stay warm in our Winter wear!");
+            $("#slogan").text("Winter Wonderland of Fashion!");
             $("#wear").attr("src", "images/winter-wear.jpg");
             $("#logo").attr("src", "images/winter.gif");
         break;
+    
+        case "fall":
+            $("html").css("background-color", "#A81124");
+            $("#slogan").text("Sweater Weather, Better Together.");
+            $("#wear").attr("src", "images/fall-wear.jpg");
+            $("#logo").attr("src", "images/fall.gif");
+        break;
 
-    }
+        case "default":
+            $("html").css("background-color", ""); 
+            $("#slogan").text("Timeless Styles, Any Time of the Year");
+            $("#wear").attr("src", "images/300x400.png");
+            $("#logo").attr("src", "images/four-seasons.gif");
+        break;
+        }
+    });
 });
